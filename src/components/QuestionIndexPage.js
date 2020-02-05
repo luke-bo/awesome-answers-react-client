7d04e141ee979eb1255b3fef124b081e661bb141
luke.bohonis <w.bohonis@gmail.com>
2020-02-05 10:31:12 -0800

    Add delete button to question index. How to create a custom event handler and use setState to update state.

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -8,11 +8,32 @@ export class QuestionIndexPage extends Component {
 
     this.state = {
       // This copies the questions array into
-      // a new array that is stored in the state 
+      // a new array that is stored in the state
       // of this component, as the state's questions field
       // questions: data.map(question => question)
       questions: [...data]
     };
+  }
+
+  deleteQuestion(id) {
+    // To update state ALWAYS use 'setState(...)' method
+
+    // You can use setState by passing an object to its first argument.
+    // Whe the time comes, the object will be merged with the current state
+
+    // This will change whatever properties are within the current state
+    // this.setState({
+    //   questions: this.state.questions.filter(q => q.id !== id)
+    // });
+
+    // You can also use setState by giving a callback as a first argument
+    // that receives the current state an dprops as arguments.
+    // It must return an object that will be merge with the state
+    this.setState((state, props) => {
+      return {
+        questions: state.questions.filter(q => q.id !== id)
+      };
+    });
   }
 
   render() {
@@ -28,6 +49,9 @@ export class QuestionIndexPage extends Component {
           {this.state.questions.map(question => (
             <li key={question.id} style={{ padding: "0.2em" }}>
               <a href="">{question.title}</a>
+              <button onClick={() => this.deleteQuestion(question.id)}>
+                Delete
+              </button>
             </li>
           ))}
         </ul>
