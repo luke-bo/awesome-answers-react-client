1ba3494f6a885370fd9647bb9019dcd47340cfc1
luke.bohonis <w.bohonis@gmail.com>
2020-02-05 09:23:16 -0800

    Change questions show and index to classical components.

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 
 import data from "../questionData";
 
-export const QuestionIndexPage = props => {
-  return (
-    <main>
-      <h2>Questions</h2>
-      <ul
-        style={{
-          listStyle: "none",
-          paddigLeft: 0
-        }}
-      >
-        {data.map(question => (
-          <li key={question.id} style={{ padding: "0.2em" }}>
-            <a href="">{question.title}</a>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
-};
+export class QuestionIndexPage extends Component {
+  render() {
+    return (
+      <main>
+        <h2>Questions</h2>
+        <ul
+          style={{
+            listStyle: "none",
+            paddigLeft: 0
+          }}
+        >
+          {data.map(question => (
+            <li key={question.id} style={{ padding: "0.2em" }}>
+              <a href="">{question.title}</a>
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+  }
+}
--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./css/QuestionShowPage.css";
 import { QuestionDetails } from "./QuestionDetails";
 import { AnswerList } from "./AnswerList";
 import oneQuestionData from "../oneQuestionData";
 
-const QuestionShowPage = () => {
-  return (
-    <div className="Page">
-      <QuestionDetails
-        title="What is your favourite color?"
-        body="Red, Green, Magenta, etc."
-        author={{ full_name: "Michael Owen" }}
-        view_count={100}
-        created_at={new Date()}
-      />
-      <AnswerList answers={oneQuestionData.answers} />
-    </div>
-  );
-};
+class QuestionShowPage extends Component {
+  render() {
+    return (
+      <div className="Page">
+        <QuestionDetails
+          title="What is your favourite color?"
+          body="Red, Green, Magenta, etc."
+          author={{ full_name: "Michael Owen" }}
+          view_count={100}
+          created_at={new Date()}
+        />
+        <AnswerList answers={oneQuestionData.answers} />
+      </div>
+    );
+  }
+}
 
 export default QuestionShowPage;
