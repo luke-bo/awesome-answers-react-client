6f5e6710125c554ad84f53188da464852b440b85
luke.bohonis <w.bohonis@gmail.com>
2020-02-05 09:35:42 -0800

    Refactor question show component to use state. Refactor jsx return to pull props from state dynamically.

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -6,17 +6,31 @@ import { AnswerList } from "./AnswerList";
 import oneQuestionData from "../oneQuestionData";
 
 class QuestionShowPage extends Component {
+  constructor(props) {
+    // When using a constructor in a class-based
+    // component, you must call the 'Component' class
+    // constructor with 'super' passing it the 'props'
+    super(props);
+    this.state = {
+      question: oneQuestionData
+    };
+  }
   render() {
+    debugger;
     return (
       <div className="Page">
         <QuestionDetails
-          title="What is your favourite color?"
-          body="Red, Green, Magenta, etc."
-          author={{ full_name: "Michael Owen" }}
-          view_count={100}
-          created_at={new Date()}
+          // verbose way to explicitly pass each prop
+          // id={this.state.}
+          // title={this.state.question.title}
+          // body={this.state.question.body}
+          // author={this.state.question.author}
+          // view_count={this.state.question.view_count}
+          // created_at={this.state.question.created_at}
+          // destructuring syntax sugar
+          {...this.state.question}
         />
-        <AnswerList answers={oneQuestionData.answers} />
+        <AnswerList answers={this.state.question.answers} />
       </div>
     );
   }
