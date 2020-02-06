2c6c727d97981c10164968416904b1b2ba0ffb5f
luke.bohonis <w.bohonis@gmail.com>
2020-02-06 10:54:27 -0800

    Install react-router. Added router components to App.js to demo basic react-router implementation.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import QuestionShowPage from "./QuestionShowPage";
 import { QuestionIndexPage } from "./QuestionIndexPage";
+import { WelcomePage } from "./WelcomePage";
 import { Clock } from "./Clock";
 
 const App = () => {
   return (
-    <div className="ui contianer segment">
-      <Clock />
-      <QuestionIndexPage />
-      <QuestionShowPage />
-    </div>
+    <BrowserRouter>
+      <div className="ui contianer segment">
+        <Clock />
+        <Route exact path="/" component={WelcomePage} />
+        <Route exact path="/questions" component={QuestionIndexPage} />
+        <Route path="/questions/:id" component={QuestionShowPage} />
+      </div>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/WelcomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const WelcomePage = props => (
+  <div className="Page">
+    <div className="ui header">Awesome Answers</div>
+    <div className="ui teal segment">Welcome to Awesome Answers React</div>
+  </div>
+);
