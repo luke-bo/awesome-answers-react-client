5441cb96242416f22c216687dab1256f3628414e
luke.bohonis <w.bohonis@gmail.com>
2020-02-07 11:45:46 -0800

    Add a create api request in user.js for SignUp.

--- src/api/user.js
+++ src/api/user.js
@@ -6,5 +6,15 @@ export const User = {
       method: "GET",
       credentials: "include"
     }).then(res => res.json());
+  },
+  create(params) {
+    return fetch(`${baseUrl}/users`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(params)
+    }).then(res => res.json());
   }
 };
