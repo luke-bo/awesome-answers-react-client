17b2f2a4a194d81ccf2ead0ab37703caf29e2951
luke.bohonis <w.bohonis@gmail.com>
2020-02-10 12:23:11 -0800

    Refactor QuestionShowPage to implement useReducer hook instead of useState hook.

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 
 import "./css/QuestionShowPage.css";
 import { QuestionDetails } from "./QuestionDetails";
@@ -8,40 +8,69 @@ import { Spinner } from "./Spinner";
 
 export const QuestionShowContext = React.createContext();
 
+const initialState = {
+  question: null,
+  isLoading: true
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_QUESTION":
+      return {
+        question: action.payload,
+        isLoading: false
+      };
+
+    case "DELETE_QUESTION":
+      return {
+        question: null,
+        isLoading: true
+      };
+
+    case "DELETE_ANSWER":
+      return {
+        ...state,
+        question: action.payload
+      };
+  }
+};
+
 const QuestionShowPage = props => {
-  const [questionShow, setQuestionShow] = useState({
-    question: null,
-    isLoading: true
-  });
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const currentQuestionId = props.match.params.id;
 
   const deleteQuestion = () => {
-    Question.destroy(questionShow.question.id).then(data => {
+    Question.destroy(state.question.id).then(data => {
+      dispatch({ type: "DELETE_QUESTION" });
       props.history.push("/questions");
     });
   };
 
   const deleteAnswer = id => {
-    const newAnswers = questionShow.question.answers.filter(a => a.id !== id);
-    setQuestionShow({
-      ...questionShow,
-      question: { ...questionShow.question, answers: newAnswers }
-    });
+    const newAnswers = state.question.answers.filter(a => a.id !== id);
+
+    const payload = {
+      ...state.question,
+      answers: newAnswers
+    };
+
+    dispatch({ type: "DELETE_ANSWER", payload });
   };
 
   useEffect(() => {
     Question.one(currentQuestionId).then(question => {
-      setQuestionShow({ question, isLoading: false });
+      // setQuestionShow({ question, isLoading: false });
+      dispatch({ type: "FETCH_QUESTION", payload: question });
     });
   }, [currentQuestionId]);
 
-  if (questionShow.isLoading) {
+  if (state.isLoading) {
     return <Spinner message="Question doesn't exist" />;
   }
   return (
     <div className="Page">
-      <QuestionDetails {...questionShow.question} />
+      <QuestionDetails {...state.question} />
       <button
         className="ui small right floated red button"
         onClick={() => deleteQuestion()}
@@ -49,7 +78,7 @@ const QuestionShowPage = props => {
         Delete
       </button>
       <QuestionShowContext.Provider value={deleteAnswer}>
-        <AnswerList answers={questionShow.question.answers} />
+        <AnswerList answers={state.question.answers} />
       </QuestionShowContext.Provider>
     </div>
   );
